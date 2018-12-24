95d4eedaaa82a6e13bfae8eb9fd343276a0164fb
Jorge Henrique <jhrocha.sousa@gmail.com>
2018-12-24 10:38:26 -0300

    .

--- Ruby/rvm_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -f /.rvm_started ]; then
-    rvm --version && ruby --version
-    exit 0
-fi
-
-source /usr/local/rvm/scripts/rvm
-rvm --version && ruby --version
-touch /.rvm_started
